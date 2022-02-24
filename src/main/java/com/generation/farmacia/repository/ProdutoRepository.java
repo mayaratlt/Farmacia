33f8cc94f47f4e3fd6e40e05a8ee4063adc3357a
mayaratlt <mayaratlt23@gmail.com>
2022-02-24 19:35:55 -0300

    Farmacia

---
+++ src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,22 @@
+package com.generation.farmacia.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.farmacia.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List <Produto> findAllByDescricaoContainingIgnoreCase(String descricao); 
+	
+	public List <Produto> findByPrecoBetween(BigDecimal start, BigDecimal end);
+	
+	Object findAllByNomeContainingIgnoreCase(String nome);
+	
+	public List <Produto> findAllByNomeContainingIgnoreCaseAndLaboratorioContainingIgnoreCase (String nome,String Laboratorio);
+	
+}
